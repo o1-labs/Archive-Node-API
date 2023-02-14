4000f525d0105661fe4ecd7343aa76ad8179dc07
Martin Minkov <minkovlmartin@gmail.com>
2023-02-13 17:59:07 -0800

    feat: Add full-chain CTE
    Adds a new CTE that gets the full chain of blocks. This is now used in
    the blocks CTE, with the idea that instead of selecting all blocks, we
    select the blocks that form a path from 0 to the current max height.
    There can be blocks submitted from block producers that do not make it
    into the chain and we want to ignore such blocks.

--- src/db/archive-node-adapter/queries.ts
+++ src/db/archive-node-adapter/queries.ts
@@ -1,18 +1,49 @@
 import type postgres from 'postgres';
 import { BlockStatusFilter } from '../../resolvers-types';
 
+function fullChainCTE(db_client: postgres.Sql) {
+  return db_client`
+  RECURSIVE pending_chain AS (
+    (
+      SELECT id, state_hash, parent_hash, parent_id, height, global_slot_since_genesis, global_slot_since_hard_fork, timestamp, chain_status 
+      FROM blocks b 
+      WHERE height = (SELECT max(height) FROM blocks) 
+    ) 
+    UNION ALL 
+    SELECT b.id, b.state_hash, b.parent_hash, b.parent_id, b.height, b.global_slot_since_genesis, b.global_slot_since_hard_fork, b.timestamp, b.chain_status 
+    FROM blocks b 
+    INNER JOIN pending_chain ON b.id = pending_chain.parent_id 
+    AND pending_chain.id <> pending_chain.parent_id 
+    AND pending_chain.chain_status <> 'canonical'
+  ), 
+  full_chain AS (
+    SELECT * 
+    FROM 
+      (
+        SELECT id, state_hash, parent_id, parent_hash, height, global_slot_since_genesis, global_slot_since_hard_fork, timestamp, chain_status 
+        FROM pending_chain 
+        UNION ALL 
+        SELECT id, state_hash, parent_id, parent_hash, height, global_slot_since_genesis, global_slot_since_hard_fork, timestamp, chain_status 
+        FROM blocks b 
+        WHERE chain_status = 'canonical'
+      ) AS full_chain
+  )
+  `;
+}
+
 function accountIdentifierCTE(
   db_client: postgres.Sql,
   address: string,
   tokenId: string
 ) {
   return db_client`
-    account_identifier AS (
-      SELECT id 
-      FROM account_identifiers ai
-      WHERE ai.public_key_id = (SELECT id FROM public_keys WHERE value = ${address})
-      AND ai.token_id = (SELECT id FROM tokens WHERE value = ${tokenId})
-    )`;
+  account_identifier AS 
+  (
+    SELECT id 
+    FROM account_identifiers ai
+    WHERE ai.public_key_id = (SELECT id FROM public_keys WHERE value = ${address})
+    AND ai.token_id = (SELECT id FROM tokens WHERE value = ${tokenId})
+  )`;
 }
 
 function blocksAccessedCTE(
@@ -22,43 +53,46 @@ function blocksAccessedCTE(
   from?: string
 ) {
   return db_client`
-    blocks_accessed AS (
-      SELECT *
-      FROM account_identifier ai
-      INNER JOIN accounts_accessed aa
-      ON ai.id = aa.account_identifier_id
-      INNER JOIN blocks b
-      ON aa.block_id = b.id
-      WHERE chain_status <> 'orphaned'
-      ${
-        status === BlockStatusFilter.all
-          ? db_client``
-          : db_client`AND chain_status = ${status.toLowerCase()}`
-      }
-      ${to ? db_client`AND b.height <= ${to}` : db_client``}
-      ${from ? db_client`AND b.height >= ${from}` : db_client``}
-    )`;
+  blocks_accessed AS 
+  (
+    SELECT *
+    FROM account_identifier ai
+    INNER JOIN accounts_accessed aa
+    ON ai.id = aa.account_identifier_id
+    INNER JOIN full_chain b
+    ON aa.block_id = b.id
+    WHERE 1 = 1
+    ${
+      status === BlockStatusFilter.all
+        ? db_client``
+        : db_client`AND chain_status = ${status.toLowerCase()}`
+    }
+    ${to ? db_client`AND b.height <= ${to}` : db_client``}
+    ${from ? db_client`AND b.height >= ${from}` : db_client``}
+  )`;
 }
 
 function emittedZkAppCommandsCTE(db_client: postgres.Sql) {
   return db_client`
-    emitted_zkapp_commands AS (
-      SELECT *
-      FROM blocks_accessed
-      INNER JOIN blocks_zkapp_commands bzkc
-      ON blocks_accessed.block_id = bzkc.block_id
-      INNER JOIN zkapp_commands zkc
-      ON bzkc.zkapp_command_id = zkc.id
-      INNER JOIN zkapp_account_update zkcu
-      ON zkcu.id = ANY(zkc.zkapp_account_updates_ids)
-      INNER JOIN zkapp_account_update_body zkcu_body
-      ON zkcu_body.id = zkcu.body_id
-    )`;
+  emitted_zkapp_commands AS 
+  (
+    SELECT *
+    FROM blocks_accessed
+    INNER JOIN blocks_zkapp_commands bzkc
+    ON blocks_accessed.block_id = bzkc.block_id
+    INNER JOIN zkapp_commands zkc
+    ON bzkc.zkapp_command_id = zkc.id
+    INNER JOIN zkapp_account_update zkcu
+    ON zkcu.id = ANY(zkc.zkapp_account_updates_ids)
+    INNER JOIN zkapp_account_update_body zkcu_body
+    ON zkcu_body.id = zkcu.body_id
+  )`;
 }
 
 function emittedEventsCTE(db_client: postgres.Sql) {
   return db_client`
-  emitted_events AS (
+  emitted_events AS 
+  (
     SELECT *
     FROM emitted_zkapp_commands
     INNER JOIN zkapp_events zke
@@ -93,12 +127,16 @@ export function getEventsQuery(
   from?: string
 ) {
   return db_client`
-    WITH ${accountIdentifierCTE(db_client, address, tokenId)},
-    ${blocksAccessedCTE(db_client, status, to, from)},
-    ${emittedZkAppCommandsCTE(db_client)},
-    ${emittedEventsCTE(db_client)}
-    SELECT *
-    FROM emitted_events`;
+  WITH 
+  ${fullChainCTE(db_client)},
+  ${accountIdentifierCTE(db_client, address, tokenId)},
+  ${blocksAccessedCTE(db_client, status, to, from)},
+  ${emittedZkAppCommandsCTE(db_client)},
+  ${emittedEventsCTE(db_client)}
+  SELECT *
+  FROM emitted_events
+  ORDER BY timestamp DESC, state_hash DESC
+  `;
 }
 
 export function getActionsQuery(
@@ -110,10 +148,14 @@ export function getActionsQuery(
   from?: string
 ) {
   return db_client`
-    WITH ${accountIdentifierCTE(db_client, address, tokenId)},
-    ${blocksAccessedCTE(db_client, status, to, from)},
-    ${emittedZkAppCommandsCTE(db_client)},
-    ${emittedActionsCTE(db_client)}
-    SELECT *
-    FROM emitted_actions`;
+  WITH 
+  ${fullChainCTE(db_client)},
+  ${accountIdentifierCTE(db_client, address, tokenId)},
+  ${blocksAccessedCTE(db_client, status, to, from)},
+  ${emittedZkAppCommandsCTE(db_client)},
+  ${emittedActionsCTE(db_client)}
+  SELECT *
+  FROM emitted_actions
+  ORDER BY height DESC
+  `;
 }
