a1b61eeebdfde05adf1bb247b0aca3aabe0b90c7
Martin Minkov <minkovlmartin@gmail.com>
2023-02-22 14:51:35 -0800

    feat: Add better error message when trying to connect to DB

--- src/context.ts
+++ src/context.ts
@@ -6,7 +6,13 @@ export interface GraphQLContext {
 
 export async function buildContext(connectionString: string | undefined) {
   const db_client = new ArchiveNodeAdapter(connectionString);
-  await db_client.checkSQLSchema();
+  try {
+    await db_client.checkSQLSchema();
+  } catch (e) {
+    throw new Error(
+      `Could not connect to Postgres with the specified connection string. Please check that Postgres is available and that your connection string is correct and try again.\nReason: ${e}`
+    );
+  }
   return {
     db_client,
   };
