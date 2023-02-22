0ea247c8ee2c370b9023164e4139566fbb87cc55
Martin Minkov <minkovlmartin@gmail.com>
2023-02-22 12:44:23 -0800

    feat: Tests for getElementIdFieldValues

--- tests/archive-node-adapter.test.ts
+++ tests/archive-node-adapter.test.ts
@@ -18,49 +18,65 @@ class ArchiveNodeAdapterExtend extends ArchiveNodeAdapter {
   partitionBlocksExtended(rows: postgres.RowList<postgres.Row[]>) {
     return this.partitionBlocks(rows);
   }
+
+  getElementIdFieldValuesExtended(rows: postgres.RowList<postgres.Row[]>) {
+    return this.getElementIdFieldValues(rows);
+  }
 }
 
 const archiveNodeAdapter = new ArchiveNodeAdapterExtend(PG_CONN);
 
 describe('ArchiveNodeAdapter', async () => {
-  test('partitionBlocks should return a non-empty map', async () => {
-    const blocksMap = archiveNodeAdapter.partitionBlocksExtended(
-      database_mock as any
-    );
-    expect(blocksMap.size).toBeTruthy();
+  describe('partitionBlocks', async () => {
+    test('partitionBlocks should return a non-empty map', async () => {
+      const blocksMap = archiveNodeAdapter.partitionBlocksExtended(
+        database_mock as any
+      );
+      expect(blocksMap.size).toBeTruthy();
+    });
+
+    test('partitionBlocks values should be able to create blockInfo', async () => {
+      const blocksMap = archiveNodeAdapter.partitionBlocksExtended(
+        database_mock as any
+      );
+
+      const blockHash = blocksMap.keys().next().value;
+      const blockRows = blocksMap.get(blockHash);
+      const blockInfo = createBlockInfo(blockRows![0]);
+
+      expect(blockInfo.height).toBeTruthy();
+      expect(blockInfo.stateHash).toBeTruthy();
+      expect(blockInfo.ledgerHash).toBeTruthy();
+      expect(blockInfo.chainStatus).toBeTruthy();
+      expect(blockInfo.timestamp).toBeTruthy();
+      expect(blockInfo.globalSlotSinceGenesis).toBeTruthy();
+      expect(blockInfo.globalSlotSinceHardfork).toBeTruthy();
+      expect(blockInfo.distanceFromMaxBlockHeight).toBeTruthy();
+    });
+
+    test('partitionBlocks values should be able to create transactionInfo', async () => {
+      const blocksMap = archiveNodeAdapter.partitionBlocksExtended(
+        database_mock as any
+      );
+
+      const blockHash = blocksMap.keys().next().value;
+      const blockRows = blocksMap.get(blockHash);
+      const transactionInfo = createTransactionInfo(blockRows![0]);
+
+      expect(transactionInfo.authorizationKind).toBeTruthy();
+      expect(transactionInfo.hash).toBeTruthy();
+      expect(transactionInfo.memo).toBeTruthy();
+      expect(transactionInfo.status).toBeTruthy();
+    });
   });
 
-  test('partitionBlocks values should be able to create blockInfo', async () => {
-    const blocksMap = archiveNodeAdapter.partitionBlocksExtended(
-      database_mock as any
-    );
-
-    const blockHash = blocksMap.keys().next().value;
-    const blockRows = blocksMap.get(blockHash);
-    const blockInfo = createBlockInfo(blockRows![0]);
-
-    expect(blockInfo.height).toBeTruthy();
-    expect(blockInfo.stateHash).toBeTruthy();
-    expect(blockInfo.ledgerHash).toBeTruthy();
-    expect(blockInfo.chainStatus).toBeTruthy();
-    expect(blockInfo.timestamp).toBeTruthy();
-    expect(blockInfo.globalSlotSinceGenesis).toBeTruthy();
-    expect(blockInfo.globalSlotSinceHardfork).toBeTruthy();
-    expect(blockInfo.distanceFromMaxBlockHeight).toBeTruthy();
-  });
-
-  test('partitionBlocks values should be able to create transactionInfo', async () => {
-    const blocksMap = archiveNodeAdapter.partitionBlocksExtended(
-      database_mock as any
-    );
-
-    const blockHash = blocksMap.keys().next().value;
-    const blockRows = blocksMap.get(blockHash);
-    const transactionInfo = createTransactionInfo(blockRows![0]);
-
-    expect(transactionInfo.authorizationKind).toBeTruthy();
-    expect(transactionInfo.hash).toBeTruthy();
-    expect(transactionInfo.memo).toBeTruthy();
-    expect(transactionInfo.status).toBeTruthy();
+  describe('getElementIdFieldValues', async () => {
+    test('getElementIdFieldValues should return a non-empty map', async () => {
+      const elementIdFieldValues =
+        archiveNodeAdapter.getElementIdFieldValuesExtended(
+          database_mock as any
+        );
+      expect(elementIdFieldValues.size).toBeTruthy();
+    });
   });
 });
