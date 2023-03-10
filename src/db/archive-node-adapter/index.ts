0030d9999edd926d4da5b1e0396d238dc33c61c0
Martin Minkov <minkovlmartin@gmail.com>
2023-03-10 10:15:37 -0800

    Add actionState field to Actions resolver (#58)
    ## Description
    
    To integrate actions into SnarkyJS, we need the actionState of an
    applied account update. We only fetch the most recent actionHash from
    the Archive Node DB since other hashes at any point in time might
    reflect an older state with some actions not included.

--- src/db/archive-node-adapter/index.ts
+++ src/db/archive-node-adapter/index.ts
@@ -110,6 +110,8 @@ export class ArchiveNodeAdapter implements DatabaseAdapter {
       traceInfo.ctx
     );
     const rows = await this.executeActionsQuery(input);
+    console.log(rows);
+
     sqlSpan?.end();
 
     const actionsProcessingSpan = traceInfo?.tracer.startSpan(
@@ -225,6 +227,7 @@ export class ArchiveNodeAdapter implements DatabaseAdapter {
   ) {
     const actionsData: Actions = [];
     for (const [, blocks] of blocksMap) {
+      const { action_state_value } = blocks[0];
       const blockInfo = createBlockInfo(blocks[0]);
       const transactionInfo = createTransactionInfo(blocks[0]);
       const actions = this.mapActionOrEvent(
@@ -232,7 +235,12 @@ export class ArchiveNodeAdapter implements DatabaseAdapter {
         blocks,
         elementIdFieldValues
       ) as Action[];
-      actionsData.push({ blockInfo, transactionInfo, actionData: actions });
+      actionsData.push({
+        blockInfo,
+        transactionInfo,
+        actionData: actions,
+        actionState: action_state_value,
+      });
     }
     return actionsData;
   }
