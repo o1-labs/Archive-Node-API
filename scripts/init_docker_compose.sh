2baa12208f5078b0d56fba3e390b92f46b285bc1
Martin Minkov <minkovlmartin@gmail.com>
2023-07-06 12:53:28 -0700

    chore(init_docker_compose.sh): improve readability by adding comments

--- scripts/init_docker_compose.sh
+++ scripts/init_docker_compose.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-set -e
+# This script is used to initialize the docker-compose Archive Node.
+# It will create a user and database in Postgres, and import the SQL dump file.
 
+set -e
 psql -U "${PGUSER}" "${PGDATABASE}" -c "CREATE USER zkapps_monitor;"
 psql -U "${PGUSER}" "${PGDATABASE}" < /data/"${PG_DUMP}"
