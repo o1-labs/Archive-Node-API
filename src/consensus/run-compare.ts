45a5cc5a0bd1aa7bbe04b709eb82bd7d9b540f60
Martin Minkov <minkovlmartin@gmail.com>
2023-07-28 16:31:52 -0700

    feat(run-compare.ts): add new file to compare TypeScript and OCaml files
    This new file, run-compare.ts, is created to compare the contents of TypeScript and OCaml files. It checks if the directories contain the same number of files, if the file names match, and if the file contents are equal. This is done to ensure consistency between TypeScript and OCaml files.

---
+++ src/consensus/run-compare.ts
@@ -0,0 +1,65 @@
+import pwd from 'process';
+import path from 'path';
+import fs from 'fs/promises';
+
+// TODO: Make these paths configurable
+const TYPESCRIPT_DIR = path.join(pwd.cwd(), 'compare-ts');
+const OCAML_DIR = path.join(pwd.cwd(), 'compare-ocaml');
+
+async function compareFiles() {
+  // Get all file names in each directory
+  const typescriptFiles = await fs.readdir(TYPESCRIPT_DIR);
+  const ocamlFiles = await fs.readdir(OCAML_DIR);
+
+  // Ensure that both directories contain the same number of files
+  if (typescriptFiles.length !== ocamlFiles.length) {
+    console.error('Directories do not contain the same number of files!');
+    console.error(`TypeScript: ${typescriptFiles.length}`);
+    console.error(`OCaml: ${ocamlFiles.length}`);
+    return;
+  }
+
+  // Iterate through each file
+  for (let i = 0; i < typescriptFiles.length; i++) {
+    const tsFile = typescriptFiles[i];
+    const ocamlFile = ocamlFiles[i];
+
+    // Ensure we're comparing files with the same name
+    if (tsFile !== ocamlFile) {
+      console.error(`File names do not match! (${tsFile} vs ${ocamlFile})`);
+      continue;
+    }
+
+    // Read each file's contents
+    const tsContent = await fs.readFile(
+      path.join(TYPESCRIPT_DIR, tsFile),
+      'utf-8'
+    );
+    const tsContentJson = JSON.parse(tsContent);
+    const ocamlContent = await fs.readFile(
+      path.join(OCAML_DIR, ocamlFile),
+      'utf-8'
+    );
+    const ocamlContentJson = JSON.parse(ocamlContent);
+
+    // Check if the file contents are equal
+    if (tsContentJson['height'] !== ocamlContentJson['height']) {
+      console.error(`File contents do not match! (${tsFile}), (${ocamlFile}})`);
+      console.error(`TypeScript: ${tsContentJson}`);
+      console.error(`OCaml: ${ocamlContentJson}`);
+    }
+
+    if (
+      tsContentJson['previous_state_hash'] !==
+      ocamlContentJson['previous_state_hash']
+    ) {
+      console.error(`File contents do not match! (${tsFile}), (${ocamlFile}})`);
+      console.error(`TypeScript: ${tsContentJson}`);
+      console.error(`OCaml: ${ocamlContentJson}`);
+    }
+  }
+  console.log('🎉 All files match!');
+}
+
+// Call the function
+compareFiles();
