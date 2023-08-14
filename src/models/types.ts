d8ed0b44ef063cd21e45efad8db5529a5da16c24
Martin Minkov <minkovlmartin@gmail.com>
2023-08-14 14:53:34 -0700

    refactor(types.ts): simplify BlocksWithTransactionsMap type definition by introducing Transaction type for better readability and maintainability

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,10 +1,9 @@
 export const DEFAULT_TOKEN_ID =
   'wSHV2S4qX9jFsLjQo8r1BsMLH2ZRKsZx6EJd1sbozGPieEC4Jf' as const;
 
-export type BlocksWithTransactionsMap = Map<
-  string,
-  Map<string, ArchiveNodeDatabaseRow[]>
->;
+export type Transaction = Map<string, ArchiveNodeDatabaseRow[]>;
+
+export type BlocksWithTransactionsMap = Map<string, Transaction>;
 
 export type FieldElementIdWithValueMap = Map<string, string>;
 
