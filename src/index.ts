42c094ab2bf523af310df530b0907e15338a505a
Martin Minkov <minkovlmartin@gmail.com>
2023-08-30 13:27:06 -0700

    feat(index.ts): wrap main function in try-catch block to handle potential errors fix(index.ts): change process.exit code from 1 to 0 on normal termination to follow standard conventions feat(index.ts): add error logging in catch block to improve error visibility and debugging

--- src/index.ts
+++ src/index.ts
@@ -7,18 +7,24 @@ import { buildServer } from './server';
 const PORT = process.env.PORT || 8080;
 
 (async function main() {
-  const context = await buildContext(process.env.PG_CONN);
-  const server = await buildServer(context);
+  try {
+    const context = await buildContext(process.env.PG_CONN);
+    const server = await buildServer(context);
 
-  ['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach((signal) => {
-    process.on(signal, () => server.close());
-  });
-  server.on('close', async () => {
-    await context.db_client.close();
-    process.exit(1);
-  });
+    server.listen(PORT, () => {
+      console.info(`Server is running on port: ${PORT}`);
+    });
 
-  server.listen(PORT, () => {
-    console.info(`Server is running on port: ${PORT}`);
-  });
+    ['SIGINT', 'SIGTERM', 'SIGQUIT'].forEach((signal) => {
+      process.on(signal, () => server.close());
+    });
+
+    server.on('close', async () => {
+      await context.db_client.close();
+      process.exit(0); // normal termination
+    });
+  } catch (error) {
+    console.error('An error occurred:', error);
+    process.exit(1); // exit with an error code
+  }
 })();
