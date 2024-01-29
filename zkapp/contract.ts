7fc7d2f009e574aa52eb761640bec4a4fe5cd570
Martin Minkov <minkovlmartin@gmail.com>
2024-01-28 18:31:14 -0800

    feat(zkapp): add new contract.ts file for smart contract logic This commit introduces a new contract.ts file in the zkapp directory. This file contains the logic for a HelloWorld smart contract, including state variables, methods, and events.
    feat(zkapp): add tsconfig.test.json for TypeScript configuration
    This commit also adds a new tsconfig.test.json file in the zkapp directory. This file contains the TypeScript configuration for the test environment.

---
+++ zkapp/contract.ts
@@ -0,0 +1,97 @@
+import {
+  Bool,
+  Field,
+  PrivateKey,
+  SmartContract,
+  State,
+  Struct,
+  UInt64,
+  method,
+  state,
+  Reducer,
+  PublicKey,
+} from 'o1js';
+
+export const adminPrivateKey = PrivateKey.random();
+export const adminPublicKey = adminPrivateKey.toPublicKey();
+
+class TestStruct extends Struct({
+  x: Field,
+  y: Bool,
+  z: UInt64,
+  address: PublicKey,
+}) {}
+
+export class HelloWorld extends SmartContract {
+  reducer = Reducer({ actionType: TestStruct });
+
+  events = {
+    singleField: Field,
+    struct: TestStruct,
+  };
+
+  @state(Field) x = State<Field>();
+  @state(Bool) y = State<Bool>();
+  @state(UInt64) z = State<UInt64>();
+  @state(Field) counter = State<Field>();
+  @state(Field) actionState = State<Field>();
+
+  init() {
+    super.init();
+    this.x.set(Field(1));
+    this.y.set(Bool(true));
+    this.z.set(UInt64.from(1));
+    this.account.delegate.set(adminPublicKey);
+  }
+
+  @method update(squared: Field, admin: PrivateKey) {
+    const x = this.x.get();
+    this.x.assertNothing();
+    x.square().assertEquals(squared);
+    this.x.set(squared);
+
+    const adminPk = admin.toPublicKey();
+
+    this.account.delegate.assertEquals(adminPk);
+  }
+
+  @method emitSingleEvent() {
+    const x = this.x.getAndRequireEquals();
+    this.emitEvent('singleField', x);
+  }
+
+  @method emitStructEvent() {
+    const x = this.x.getAndRequireEquals();
+    const y = this.y.getAndRequireEquals();
+    const z = this.z.getAndRequireEquals();
+    this.emitEvent(
+      'struct',
+      new TestStruct({ x, y, z, address: this.address })
+    );
+  }
+
+  @method emitStructAction() {
+    const x = this.x.getAndRequireEquals();
+    const y = this.y.getAndRequireEquals();
+    const z = this.z.getAndRequireEquals();
+    this.reducer.dispatch(new TestStruct({ x, y, z, address: this.address }));
+  }
+
+  @method reduceStructAction() {
+    let counter = this.counter.getAndRequireEquals();
+    let actionState = this.actionState.getAndRequireEquals();
+
+    let { actionState: newActionState } = this.reducer.reduce(
+      this.reducer.getActions({
+        fromActionState: actionState,
+      }),
+      Field,
+      (state: Field, action: TestStruct) => {
+        return state.add(action.x);
+      },
+      { state: counter, actionState }
+    );
+
+    this.actionState.set(newActionState);
+  }
+}
