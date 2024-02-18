e56470662a7e26393709d7287e02740f55df01b9
Martin Minkov <minkovlmartin@gmail.com>
2024-02-18 15:09:08 -0800

    Add lightnet to tests (#99)

--- tests/resolvers.test.ts
+++ tests/resolvers.test.ts
@@ -1,11 +1,22 @@
-import { expect, test, describe, beforeAll } from 'vitest';
+import { test, before, describe, after } from 'node:test';
+import assert from 'node:assert';
 import { createYoga, createSchema } from 'graphql-yoga';
 import { loadSchemaSync } from '@graphql-tools/load';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
 import { buildHTTPExecutor } from '@graphql-tools/executor-http';
 import { parse } from 'graphql';
-import { resolvers } from '../src/resolvers';
-import { buildContext, GraphQLContext } from '../src/context';
+import { PrivateKey, Lightnet } from 'o1js';
+import { resolvers } from '../src/resolvers.js';
+import { buildContext, GraphQLContext } from '../src/context.js';
+import {
+  deployContract,
+  emitAction,
+  emitMultipleFieldsEvent,
+  emitSingleEvent,
+  setNetworkConfig,
+  Keypair,
+} from '../zkapp/utils.js';
+import { HelloWorld } from '../zkapp/contract.js';
 
 const eventsQuery = `
 query getEvents($input: EventFilterOptionsInput!) {
@@ -64,16 +75,18 @@ query getActions($input: ActionFilterOptionsInput!) {
 }
 `;
 
-const address = 'B62qngHtRemqzpwhrpohji5K1iTknVdHXyWvDyRpXFhjHZ1JF4uaZZC';
-
-const PG_CONN = process.env.GITHUB_ACTIONS
-  ? 'postgres://postgres:password@postgres:5432/archive'
-  : 'postgres://postgres:password@localhost:5432/archive';
+// This is the default connection string provided by the lightnet postgres container
+const PG_CONN = 'postgresql://postgres:postgres@localhost:5432/archive ';
 
 describe('Query Resolvers', async () => {
-  let executor;
+  let executor: any;
+  let senderKeypair: Keypair;
+  let zkAppKeypair: Keypair;
+  let zkApp: HelloWorld;
 
-  beforeAll(async () => {
+  before(async () => {
+    setNetworkConfig();
+
     const schema = createSchema({
       typeDefs: loadSchemaSync('./schema.graphql', {
         loaders: [new GraphQLFileLoader()],
@@ -85,20 +98,30 @@ describe('Query Resolvers', async () => {
     executor = buildHTTPExecutor({
       fetch: yoga.fetch,
     });
+
+    const zkAppKey = PrivateKey.random();
+    zkAppKeypair = {
+      privateKey: zkAppKey,
+      publicKey: zkAppKey.toPublicKey(),
+    };
+    senderKeypair = await Lightnet.acquireKeyPair();
+    zkApp = await deployContract(zkAppKeypair, senderKeypair);
   });
 
-  describe('Events', () => {
-    test('Fetching events with a valid address should not throw', async () => {
-      expect(async () => {
+  after(async () => {
+    process.exit(0);
+  });
+
+  describe('Events', async () => {
+    test('Fetching events with a valid address but no emitted events should not throw', async () => {
+      assert.doesNotThrow(async () => {
         await executor({
           variables: {
-            input: {
-              address,
-            },
+            input: { address: zkAppKeypair.publicKey.toBase58() },
           },
           document: parse(`${eventsQuery}`),
         });
-      }).not.toThrowError();
+      });
     });
 
     test('Fetching events with a empty address should return empty list', async () => {
@@ -110,22 +133,83 @@ describe('Query Resolvers', async () => {
         },
         document: parse(`${eventsQuery}`),
       });
-      expect(results.data.events).toStrictEqual([]);
+      assert.strictEqual(results.data.events.length, 0);
+    });
+
+    test('Emitting an event with single field should return a single event with the correct data', async () => {
+      await emitSingleEvent(zkApp, senderKeypair);
+      const results = await executor({
+        variables: {
+          input: {
+            address: zkAppKeypair.publicKey.toBase58(),
+          },
+        },
+        document: parse(`${eventsQuery}`),
+      });
+
+      const events = results.data.events;
+      const lastEvent = events[events.length - 1];
+      assert.strictEqual(lastEvent.eventData.length, 1);
+    });
+
+    test('Emitting multiple events with a single field should return multiple events with the correct data', async () => {
+      await emitSingleEvent(zkApp, senderKeypair, { numberOfEmits: 3 });
+      const results = await executor({
+        variables: {
+          input: {
+            address: zkAppKeypair.publicKey.toBase58(),
+          },
+        },
+        document: parse(`${eventsQuery}`),
+      });
+      const events = results.data.events;
+      const lastEvent = events[events.length - 1];
+      assert.strictEqual(lastEvent.eventData.length, 3);
+    });
+
+    test('Emitting an event with multiple fields should return an event with multiple values', async () => {
+      await emitMultipleFieldsEvent(zkApp, senderKeypair);
+      const results = await executor({
+        variables: {
+          input: {
+            address: zkAppKeypair.publicKey.toBase58(),
+          },
+        },
+        document: parse(`${eventsQuery}`),
+      });
+      const events = results.data.events;
+      const lastEvent = events[events.length - 1];
+      assert.strictEqual(lastEvent.eventData.length, 1);
+    });
+
+    test('Emitting multiple events with multiple fields should return multiple events with the correct data', async () => {
+      await emitMultipleFieldsEvent(zkApp, senderKeypair, { numberOfEmits: 3 });
+      const results = await executor({
+        variables: {
+          input: {
+            address: zkAppKeypair.publicKey.toBase58(),
+          },
+        },
+        document: parse(`${eventsQuery}`),
+      });
+      const events = results.data.events;
+      const lastEvent = events[events.length - 1];
+      assert.strictEqual(lastEvent.eventData.length, 3);
     });
   });
 
-  describe('Actions', () => {
+  describe('Actions', async () => {
     test('Fetching actions with a valid address should not throw', async () => {
-      expect(async () => {
+      assert.doesNotThrow(async () => {
         await executor({
           variables: {
             input: {
-              address,
+              address: zkAppKeypair.publicKey.toBase58(),
             },
           },
           document: parse(`${actionsQuery}`),
         });
-      }).not.toThrowError();
+      });
     });
 
     test('Fetching actions with a empty address should return empty list', async () => {
@@ -137,7 +221,37 @@ describe('Query Resolvers', async () => {
         },
         document: parse(`${actionsQuery}`),
       });
-      expect(results.data.actions).toStrictEqual([]);
+      assert.strictEqual(results.data.actions.length, 0);
+    });
+
+    test('Emitting an action should return a single action with the correct data', async () => {
+      await emitAction(zkApp, senderKeypair);
+      const results = await executor({
+        variables: {
+          input: {
+            address: zkAppKeypair.publicKey.toBase58(),
+          },
+        },
+        document: parse(`${actionsQuery}`),
+      });
+      const actions = results.data.actions;
+      const lastAction = actions[actions.length - 1];
+      assert.strictEqual(lastAction.actionData.length, 1);
+    });
+
+    test('Emitting multiple actions should return multiple actions with the correct data', async () => {
+      await emitAction(zkApp, senderKeypair, { numberOfEmits: 3 });
+      const results = await executor({
+        variables: {
+          input: {
+            address: zkAppKeypair.publicKey.toBase58(),
+          },
+        },
+        document: parse(`${actionsQuery}`),
+      });
+      const actions = results.data.actions;
+      const lastAction = actions[actions.length - 1];
+      assert.strictEqual(lastAction.actionData.length, 3);
     });
   });
 });
