e7aacc0b39c6acfd5bf9f1b9cbd12ad8bf320143
Coby <coby@o1labs.org>
2025-01-21 16:42:25 -0500

    fix linting error

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,6 @@
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { loadSchemaSync } from '@graphql-tools/load';
 import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
-import { GraphQLError } from 'graphql';
 
 import { Resolvers } from './resolvers-types.js';
 import {
